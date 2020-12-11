ba45fb726c9d5cf8674fef32bd0d248278c1829c
Bruno Guerra <gguerrabruno@outlook.com>
2020-12-11 15:32:40 -0300

    feat
    Theme logic

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import Router from './src/Router';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { AuthProvider } from './src/stores/AuthContext';
+import { useColorScheme } from 'react-native';
+
+import Router from './src/Router';
 
 export default function App() {
 
+  const colorScheme = useColorScheme();
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-        <Router/>
+        <Router />
       </AuthProvider>
     </NavigationContainer>
   );
